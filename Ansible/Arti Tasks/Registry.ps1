77afd4d0e0c3ed72c12aa511a5308e8196b8ae1c
arti <arti04294@gmail.com>
2020-03-30 13:31:00 +0530

    Ansible Scrits Cmomitted

---
+++ Ansible/Arti Tasks/Registry.ps1
@@ -0,0 +1,5 @@
+﻿
+$cred= Get-Credential
+invoke-Command -ComputerName "ip address of server" -Credential $cred -ScriptBlock {Get-Item “HKLM:\Software\Microsoft\Windows\CurrentVersion\Run”}
+
+ 
---
+++ Ansible/Arti Tasks/remotecopycerts.ps1
@@ -0,0 +1,12 @@
+﻿New-SelfSignedCertificate -DnsName "mycertdns" -CertStoreLocation "cert:\LocalMachine\My"
+$cred = Get-credential 
+$sess = New-PSSession -ComputerName "ip address of server" -Credential $cred
+$pathcert = "Cert:\LocalMachine\My"
+Get-ChildItem -Path $pathcert 
+$thumbprint= "thumbprintofcert"
+$cert = (Get-ChildItem -Path $certpath\$thumbprint)
+Export-Certificate -Cert $cert -FilePath "path to be extractedr"
+copy-item -Path "path of extracted cert" -Destination "path of cert to be copied" -ToSession $sess -Recurse
+
+
+
